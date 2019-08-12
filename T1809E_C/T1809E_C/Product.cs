d920ee46a30b83df4a99b54a0fcc6258dab291f0
The Nhat <nhatntth1809023@fpt.edu.vn>
2019-08-12 19:52:53 +0700

    Bai thi

--- T1809E_C/T1809E_C/Product.cs
+++ T1809E_C/T1809E_C/Product.cs
@@ -3,196 +3,93 @@ using System.Collections.Generic;
 
 namespace T1809E_C
 {
-    public  class Product
+    public class Product
     {
-        public int id;
-        public string name;
-        public decimal price;
-        public int qty;
-        public string image;
-        public string desc;
-        public List<string> gallery;
-
-        private event Notification ShowNotifyWhenChangePrice;
-        private event Notification ShowNotifyWhenChangeQty;
+        public string ProductId;
+        public string Name;
+        public decimal Price;
         
-        public Product()
+        public void AddProduct()
         {
-            this.gallery = new List<string>();
-        }
-
-        public Product(int id, string name, decimal price, int qty, 
-            string image, string desc)
-        {
-            this.id = id;
-            this.name = name;
-            this.price = price;
-            this.qty = qty;
-            this.image = image;
-            this.desc = desc;
-            this.gallery = new List<string>();
-        }
-
-        public string this[int index]
-        {
-            get
-            {
-                return "aaaa"+gallery[index];
-            }
-            set
-            {
-                if (value.Length > 10)
-                {
-                    gallery[index] = value;
-                }
-            }
-        }
-            
-        public int Id
-        {
-            get => id;
-            set => id = value;
-        }
-
-        public string Name
-        {
-            get => name;
-            set => name = value;
-        }
-
-        public decimal Price
-        {
-            get => price;
-            set
-            {
-                
-                if (ShowNotifyWhenChangePrice == null)
-                {
-                    ShowNotifyWhenChangePrice += Cart.ShowNotification;
-                }
-
-                if (this.Price != value && this.Price != 0)
-                {
-                    ShowNotifyWhenChangePrice("Gia da duoc cap nhat");
-                }
-                price = value;
-            }
-        }
-
-        public int Qty
-        {
-            get => qty;
-            set
-            {
-                if (ShowNotifyWhenChangeQty == null)
-                {
-                    ShowNotifyWhenChangeQty += Cart.ShowNotification;
-                }
-
-                if (this.Qty != value && this.Qty != 0)
-                {
-                    ShowNotifyWhenChangeQty("So luong sp moi duoc cap nhat");
-                }
-                qty = value;
-            }
-        }
-
-        public string Image
-        {
-            get => image;
-            set => image = value;
-        }
-
-        public string Desc
-        {
-            get => desc;
-            set => desc = value;
-        }
-
-        public virtual void TinhTien()
-        {
-            Product p = new Product();
-            p.gallery.Add("helloworld");
-            p.gallery.Add("goodmorning");
-            
-            Console.WriteLine(p.gallery[0]);
-            Console.WriteLine(p[0]);
-            p.gallery[1] = "hahaha";
-            p[1] = "abcxyz";
-        }
-
-        public virtual void GetInfo()
-        {
-            
-            Console.WriteLine("id: "+id);
-            Console.WriteLine("name: "+name);
-            Console.WriteLine("price: "+price);
-            Console.WriteLine("qty: "+qty);
-            Console.WriteLine("image: "+ this.Image);
-            Console.WriteLine("desc: "+desc);
-            this.Image = "abcxyz";
-            foreach (string s in gallery)
-            {
-                Console.WriteLine(s);
-            }
-        }
-
-        public bool CheckQty()
-        {
-            if (qty > 0)
-            {
-                return true;
-            }
-            return false;
-        }
-
-        public bool AddGallery(string img)
-        {
-            if (gallery.Count < 10)
-            {
-                gallery.Add(img);
-                return true;
-            }
-            Console.WriteLine("Vuot qua so anh cho phep");
-            return false;
-        }
-
-        public void AddGallery2()
-        {
-            if (gallery.Count < 10)
-            {
-                string img = Console.ReadLine();
-                gallery.Add(img);
-                return;
-            }
-            Console.WriteLine("Vuot qua so anh cho phep");
-        }
-
-        public void RemoveGallery()
-        {
-            int i = 0;
-            foreach (string s in gallery)
-            {
-                Console.WriteLine(i+". "+ s);
-                i++;
-            }
-            Console.WriteLine("Nhap vi tri anh muon xoa:");
-            int n = Convert.ToInt16(Console.ReadLine());
-            gallery.RemoveAt(n);
-        }
-
-        public bool RemoveGallery(int n)
-        {
-            if (n < gallery.Count)
-            {
-                gallery.RemoveAt(n);
-                //gallery.Remove("afafaafa");
-                return true;
-            }
-            Console.WriteLine("Khong tim thay anh hoac khong the xoa");
-            return false;
+           Console.WriteLine("ID Product:");
+           ProductId = Console.ReadLine();
+           Console.WriteLine("Name:");
+           Name = Console.ReadLine();
+           Console.WriteLine("Price:");
+           Price = Convert.ToDecimal(Console.ReadLine());
         }
         
+        public void GetProduct()
+        {
+            Console.WriteLine("ID Product:" +ProductId);
+            Console.WriteLine("Name:" +Name);
+            Console.WriteLine("Price:" +Price);
+        }
     }
 
+    public class ListProduct : Product
+    {
+        List<Product> productList =new List<Product>();
+        public void AddListProduct()
+        {
+            Console.WriteLine("Them hang");
+            Product ll=new Product();
+            ll.AddProduct();
+            productList.Add(ll);
+        }
+
+        public void ShowProduct()
+        {
+            foreach (var x in productList)
+            {
+                x.GetProduct();
+                Console.WriteLine("-------------------");
+            } 
+        }
+
+        public void DeleteProduct()
+        {
+            Console.WriteLine("Enter Product Id Delete:");
+            string Id = Console.ReadLine();
+            foreach (Product x in productList)
+            {
+                if (x.ProductId.Equals(Id))
+                {
+                    productList.Remove(x);
+                }
+            }
+        }
+    }
+
+    public class Menu
+    {
+        public static void Main(string[] args) {
+            ListProduct lk=new ListProduct();
+            int x;
+            do {
+                Console.WriteLine("\t\t----------------------------------");
+                Console.WriteLine("\t\t|\t\t\t\tMENU\t\t\t |");
+                Console.WriteLine("\t\t----------------------------------");
+                Console.WriteLine("\t\t|1. Add Product");
+                Console.WriteLine("\t\t|2. Show Product");
+                Console.WriteLine("\t\t|3. Delete Product");
+                Console.WriteLine("\t\t|4. Exit");
+                Console.WriteLine("\t\t----------------------------------");
+                x = Convert.ToInt32(Console.ReadLine());
+                switch (x) {
+                    case 1:
+                        lk.AddListProduct();
+                        break;
+                    case 2:
+                        lk.ShowProduct();
+                        break;
+                    case 3:
+                        lk.DeleteProduct();
+                        break;
+                }
+            } while (x != 4);
+            Console.WriteLine(" ");
+        }
+    }
+    
 }
